c9f96fafc25ddd1a197b08069b0e24d978b0be14
Austreelis <dev@austreelis.net>
2023-02-13 18:29:59 +0100

    [devshell] include the rp2040 emulator

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    devshell.url = "github:numtide/devshell";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firmware = {
+      url = "http://micropython.org/download/rp2-pico/rp2-pico-latest.uf2";
+      flake = false;
+    };
+    rp2040js = {
+      url = "github:wokwi/rp2040js";
+      flake = false;
+    };
   };
 
   outputs = inputs: let
-    inherit (inputs) devshell nixpkgs;
+    inherit (inputs) devshell nixpkgs self;
     inherit (nixpkgs.lib) foldl' lists mapAttrs recursiveUpdate;
 
     # Supported host systems
@@ -35,9 +46,21 @@
           
           { package = pkgs.micropython; category = "micropython"; }
           { package = pkgs.rshell; category = "micropython"; }
+
+          {
+            name = "rp2040";
+            help = "micropython emulator in Typescript by wokwi";
+            category = "micropython";
+            command = ''
+              echo "CTRL+X TO QUIT"
+              npm run start:micropython --prefix ''${PRJ_DATA_DIR}/rp2040 -- \
+                --image=${inputs.firmware} $@
+            '';
+          }
         ];
 
         devshell.packages = [
+          pkgs.nodejs-16_x
           pkgs.python310Packages.python-lsp-black
           pkgs.python310Packages.python-lsp-server
         ];
@@ -55,8 +78,22 @@
           mkdir -p "$PRJ_DATA_DIR"
           if ! [ -x "$PRJ_DATA_DIR"/venv/bin/activate ]; then
             python -m venv "$PRJ_DATA_DIR"/venv
+            chmod +x "$PRJ_DATA_DIR"/venv/bin -R
           fi
           "$PRJ_DATA_DIR"/venv/bin/activate
+        '';
+
+        # Ensure node modules are installed
+        devshell.startup.npm-install.text = ''
+          mkdir -p "$PRJ_DATA_DIR"
+          if ! [ -d "$PRJ_DATA_DIR"/rp2040 ]; then
+            cp -rn ${inputs.rp2040js} ''${PRJ_DATA_DIR}/rp2040
+            chmod +w ''${PRJ_DATA_DIR}/rp2040 -R
+            sed -i 's/"prepare": "husky install",//' ''${PRJ_DATA_DIR}/rp2040/package.json
+          fi
+          if ! [ -d "$PRJ_DATA_DIR"/rp2040/node_modules ]; then
+              npm --prefix ''${PRJ_DATA_DIR}/rp2040 install --no-audit --no-fund
+          fi
         '';
       };
     in {
